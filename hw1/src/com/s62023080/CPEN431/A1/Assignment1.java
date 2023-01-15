2617b95b4f6cb59f80bcece73fc6f47af0eb3633
Luke J. Basso <lukejbass@gmail.com>
2023-01-14 18:16:30 -0800

    Copy hw1

---
+++ hw1/src/com/s62023080/CPEN431/A1/Assignment1.java
@@ -0,0 +1,41 @@
+package com.s62023080.CPEN431.A1;
+import java.io.*;
+import java.net.*;
+import java.net.http.HttpTimeoutException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.Random;
+
+public class Assignment1 {
+    public static void main(String[] args) throws IOException {
+//        // Requires 3 arguments
+//        if (args.length != 3) {
+//            System.out.println("This requires an address, port, and student ID");
+//            return;
+//        }
+//
+        Fetch fetch = new Fetch("34.213.181.35", "43101", 100, 4);
+
+        try {
+            // Create request using student ID
+            byte[] request = new byte[4];
+            ByteBuffer buffer = ByteBuffer.wrap(request);
+            buffer.order(ByteOrder.LITTLE_ENDIAN);
+            buffer.putInt(Integer.parseInt("1381632"));
+            buffer = ByteBuffer.wrap(fetch.fetch(request));
+
+            // First 4 bytes are secret code length
+            buffer.order(ByteOrder.BIG_ENDIAN);
+            int length = buffer.getInt();
+            buffer.order(ByteOrder.LITTLE_ENDIAN);
+            // Next length bytes are secret code
+            byte[] code = new byte[length];
+            buffer.get(code);
+            System.out.println(StringUtils.byteArrayToHexString(code));
+        } catch (HttpTimeoutException e) {
+            System.out.println(e.getMessage());
+        }
+
+        fetch.close();
+    }
+}
---
+++ hw1/src/com/s62023080/CPEN431/A1/Fetch.java
@@ -0,0 +1,106 @@
+package com.s62023080.CPEN431.A1;
+import java.io.*;
+import java.net.*;
+import java.net.http.HttpTimeoutException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.Random;
+
+public class Fetch {
+    private final int ID_SIZE = 16;
+
+    private final DatagramSocket socket;
+
+    private final int timeout;
+
+    private final int retries;
+
+    public Fetch(String host, String port, int timeout, int retries) throws SocketException, UnknownHostException {
+        socket = new DatagramSocket();
+        socket.connect(InetAddress.getByName(host), Integer.parseInt(port));
+        this.timeout = timeout;
+        socket.setSoTimeout(timeout);
+        this.retries = retries;
+    }
+
+    public void close() {
+        socket.close();
+    }
+
+    public void reset() throws SocketException {
+        socket.setSoTimeout(this.timeout);
+    }
+
+    public void setTimeout(int timeout) throws SocketException {
+        socket.setSoTimeout(timeout);
+    }
+
+    public byte[] createRequestId() {
+        byte[] requestId = new byte[ID_SIZE];
+        ByteBuffer buffer = ByteBuffer.wrap(requestId);
+        buffer.order(ByteOrder.LITTLE_ENDIAN);
+
+        // First 4 bytes are client IP
+        buffer.put(socket.getLocalAddress().getAddress());
+        // Next 2 bytes are client port
+        buffer.putShort((short) socket.getLocalPort());
+        // Next 2 bytes are random
+        byte[] random = new byte[2];
+        new Random().nextBytes(random);
+        buffer.put(random);
+        // Next 8 bytes are time
+        buffer.putLong(System.nanoTime());
+
+        return requestId;
+    }
+
+    public byte[] sendReceive(byte[] request) throws IOException {
+        DatagramPacket requestPacket = new DatagramPacket(request, request.length);
+        socket.send(requestPacket);
+        DatagramPacket responsePacket = new DatagramPacket(new byte[1000], 1000);
+        socket.receive(responsePacket);
+        return responsePacket.getData();
+    }
+
+    public byte[] fetch(byte [] request) throws IOException {
+        byte[] formattedRequest = new byte[ID_SIZE + request.length];
+        ByteBuffer buffer = ByteBuffer.wrap(formattedRequest);
+        buffer.order(ByteOrder.LITTLE_ENDIAN);
+
+        // First 16 bytes are request ID
+        byte[] requestId = createRequestId();
+        buffer.put(requestId);
+        // Next length bytes are request
+        buffer.put(request);
+
+        byte[] formattedResponse = null;
+        int retries = this.retries;
+        while (retries > 0) {
+            try {
+                byte[] response = sendReceive(formattedRequest);
+                buffer = ByteBuffer.wrap(response);
+
+                // First 16 bytes are response ID
+                byte[] responseId = new byte[ID_SIZE];
+                buffer.get(responseId);
+
+                // Next length bytes are response
+                formattedResponse = new byte[response.length - ID_SIZE];
+                buffer.get(formattedResponse);
+
+                break;
+            } catch (SocketTimeoutException e) {
+                setTimeout(socket.getSoTimeout() * 2);
+                retries -= 1;
+            }
+        }
+
+        reset();
+
+        if (formattedResponse == null) {
+            throw new HttpTimeoutException("Request Failed");
+        }
+
+        return formattedResponse;
+    }
+}
