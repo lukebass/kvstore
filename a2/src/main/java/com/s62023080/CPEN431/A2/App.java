df0c065533a318c8c5085e447d621db0e4b897de
Luke J. Basso <lukejbass@gmail.com>
2023-01-18 09:39:07 -0800

    Init a2 directory

---
+++ a2/src/main/java/com/s62023080/CPEN431/A2/App.java
@@ -0,0 +1,13 @@
+package com.s62023080.CPEN431.A2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
