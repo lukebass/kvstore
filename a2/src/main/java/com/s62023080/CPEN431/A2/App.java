07cbde0cb9c8d1622dd2e15352267023c8a33f86
Luke J. Basso <lukejbass@gmail.com>
2023-01-19 14:08:00 -0800

    Move proto files and start work

--- a2/src/main/java/com/s62023080/CPEN431/A2/App.java
+++ a2/src/main/java/com/s62023080/CPEN431/A2/App.java
@@ -1,16 +1,16 @@
 package com.s62023080.CPEN431.A2;
 
+import ca.NetSysLab.ProtocolBuffers.RequestPayload.ReqPayload;
+import ca.NetSysLab.ProtocolBuffers.ResponsePayload.ResPayload;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
 public class App {
-    public static byte[] generateCode(String studentId, Fetch fetch) throws IOException {
-        byte[] request = new byte[4];
-        ByteBuffer buffer = ByteBuffer.wrap(request);
-        buffer.order(ByteOrder.LITTLE_ENDIAN);
-        buffer.putInt(Integer.parseInt(studentId));
-        return fetch.fetch(request);
+    public static byte[] generateReqPayload(String studentId) {
+        ReqPayload.Builder reqPayload = ReqPayload.newBuilder();
+        reqPayload.setStudentID(Integer.parseInt(studentId));
+        return reqPayload.build().toByteArray();
     }
 
     public static void main(String[] args) {
@@ -21,7 +21,9 @@ public class App {
 
         try {
             Fetch fetch = new Fetch(args[0], args[1], 100, 4);
-            ByteBuffer buffer = ByteBuffer.wrap(generateCode(args[2], fetch));
+            byte[] reqPayload = generateReqPayload(args[2]);
+            ResPayload resPayload = ResPayload.parseFrom(fetch.fetch(reqPayload));
+            ByteBuffer buffer = ByteBuffer.wrap(resPayload.toByteArray());
             // First 4 bytes are secret code length
             buffer.order(ByteOrder.BIG_ENDIAN);
             int length = buffer.getInt();
